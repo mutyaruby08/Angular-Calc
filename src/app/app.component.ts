8c670bd994c941561db679bd9367adf313861a84
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2021-10-02 15:04:44 +0800

    Angular Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Calc';
+
+  res = 0;
+  isNum1 = 0;
+  isNum2 = 0;
+  sum = 0;
+  sub = 0;
+  mul = 0;
+  divi = 0;
+
+
+  getNum1(value: string){
+    const parsedInt = parseInt(value);
+      
+      if(!isNaN(parsedInt)){
+        this.isNum1 = parsedInt;
+        console.log(this.isNum1);
+      }
+  }
+  getNum2(value: string){
+    const parsedInt = parseInt(value);
+      
+      if(!isNaN(parsedInt)){
+        this.isNum2 = parsedInt;
+        console.log(this.isNum2);
+      }
+  }
+
+  clickAdd(){
+    let sum = this.isNum1 + this.isNum2;
+    this.res = sum;
+  }
+
+  clickSub(){
+    let sub = this.isNum1 - this.isNum2;
+    this.res = sub;
+  }
+
+  clickMul(){
+    let mul = this.isNum1 * this.isNum2;
+    this.res = mul;
+  }
+
+  clickDiv(){
+    let divi = this.isNum1 / this.isNum2;
+    this.res = divi;
+  }
+
+
 }
